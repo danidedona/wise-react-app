41428fcbf3a6bcd9bc527de92428a4ed495bbd61
danidedona <daniela_dedona@brown.edu>
2025-10-07 16:25:08 -0400

    feat: added event

---
+++ src/pages/events/upcomingEventsData/fall-2025-wise-x-brown-research-club.jsx
@@ -0,0 +1,13 @@
+export default {
+  index: 0,
+  title: "Getting Involved in Research with Brown Research Club", // ADD FALL 2025
+  date: "October 16th, 2025",
+  time: "5:00 PM - 6:00 PM",
+  location: "Salomon 003",
+  // link: "/events/fall-2025-wise-x-brown-research-club",
+  coverImage: "/events/fall-2025-wise-x-brown-research-club/flier.png",
+  flier: "/events/fall-2025-wise-x-brown-research-club/flier.png",
+  description:
+    "Interested in getting involved in reserach at Brown? Join WiSE and Brown Research Club for a presentation and panel discussion! Refreshments provided :)",
+  imageFolder: "/events/fall-2025-wise-x-brown-research-club",
+};
