f87f6b1b36bb2fff5f13e53e8bc6ae7d1b5ec34c
danidedona <daniela_dedona@brown.edu>
2025-10-19 16:32:38 -0400

    fix: careerlab -> career center

--- src/pages/events/upcomingEventsData/fall-2025-wise-x-careerlab.jsx
+++ src/pages/events/upcomingEventsData/fall-2025-wise-x-careerlab.jsx
@@ -1,13 +1,13 @@
 export default {
   index: 2,
-  title: "Resumes and LinkedIn Workshop with Careerlab", // ADD FALL 2025
+  title: "Resumes and LinkedIn Workshop with Career Center", // ADD FALL 2025
   date: "October 30th, 2025",
   time: "6:00 PM - 7:00 PM",
   location: "Career Center",
   // link: "/events/fall-2025-wise-x-brown-research-club",
-  coverImage: "/events/fall-2025-wise-x-careerlab.png",
-  flier: "/events/fall-2025-wise-x-careerlab/flier.png",
+  coverImage: "/events/fall-2025-wise-x-career-center.png",
+  flier: "/events/fall-2025-wise-x-career-center/flier.png",
   description:
-    "Interested in applying to internships and research positions? Joine WiSE to work on your resume and LinkedIn!",
-  imageFolder: "/events/fall-2025-wise-x-careerlab",
+    "Interested in applying to internships and research positions? Join WiSE to work on your resume and LinkedIn!",
+  imageFolder: "/events/fall-2025-wise-x-career-center",
 };
