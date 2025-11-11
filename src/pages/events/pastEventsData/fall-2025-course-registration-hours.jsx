b32774723012576744bede721534c3c11a9a0263
danidedona <daniela_dedona@brown.edu>
2025-11-11 10:00:11 -0500

    set event to past

--- src/pages/events/pastEventsData/fall-2025-course-registration-hours.jsx
+++ src/pages/events/pastEventsData/fall-2025-course-registration-hours.jsx
@@ -1,11 +1,11 @@
 export default {
   index: 8,
-  title: "Course Registration Drop-In Hours",
+  title: "Fall 2025 Course Registration Drop-In Hours",
   date: "November 9th, 2025",
   time: "4:00 PM - 6:00 PM",
   location: "Kassar Foxboro Auditorium",
-  // link: "/events/fall-2025-wise-x-career-center",
-  coverImage: "/events/fall-2025-course-registration-hours.png",
+  link: "/events/fall-2025-course-registration-hours",
+  coverImage: "/events/fall-2025-course-registration-hours/flier.png",
   flier: "/events/fall-2025-course-registration-hours/flier.png",
   description:
     "Drop in to get help with course registration from WiSE mentors! Whether you're unsure about which classes to take, need advice on balancing your schedule, or have questions about the registration process, our experienced mentors are here to assist you. No appointment necessary—just come by The Underground between 4:00 PM and 6:00 PM on November 9th, 2025. We look forward to helping you navigate course registration with confidence!",
