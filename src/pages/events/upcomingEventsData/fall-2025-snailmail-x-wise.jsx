47f8ae14c5fbaa37a53550c0431e4ca7fe4198c5
danidedona <daniela_dedona@brown.edu>
2025-12-03 01:00:03 -0500

    feat: new event (snail mail x wise)

---
+++ src/pages/events/upcomingEventsData/fall-2025-snailmail-x-wise.jsx
@@ -0,0 +1,13 @@
+export default {
+  index: 0,
+  title: "Snail Mail x WiSE: De-stress Sesh!",
+  date: "December 4th, 2025",
+  time: "7:00 PM - 9:00 PM",
+  location: "Baurs and Holley 157/159",
+  // link: "/events/fall-2025-snailmail-x-wise",
+  coverImage: "/events/fall-2025-snailmail-x-wise/flier.png",
+  flier: "/events/fall-2025-snailmail-x-wise/flier.png",
+  description:
+    "Attendees enjoyed a relaxing evening of de-stressing before finals! Journaling supplies and de-stress materials were provided, along with holiday refreshments!",
+  imageFolder: "/events/fall-2025-snailmail-x-wise",
+};
