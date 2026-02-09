5e150d58b5df682eb86e3c29aa9609fec69bcd53
danidedona <daniela_dedona@brown.edu>
2026-02-08 22:34:57 -0500

    event added

--- src/pages/events/upcomingEventsData/fall-2025-wip-x-wise-x-astroclub.jsx
+++ src/pages/events/upcomingEventsData/fall-2025-wip-x-wise-x-astroclub.jsx
@@ -4,7 +4,7 @@ export default {
   date: "February 13th, 2026",
   time: "6:30 PM - 8:00 PM",
   location: "Petteruti Lounge",
-  link: "/events/spring-2026-wise-galentines",
+  // link: "/events/spring-2026-wise-galentines",
   coverImage: "/events/spring-2026-wise-galentines/flier.png",
   flier: "/events/spring-2026-wise-galentines/flier.png",
   description:
