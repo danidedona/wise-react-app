dc233a86138ef2f721446f7f26a3a3353fc7c14f
danidedona <daniela_dedona@brown.edu>
2026-02-28 21:06:25 -0500

    comment out event

--- src/pages/events/upcomingEventsData/spring-2026-concentration-declaration-panel.jsx
+++ src/pages/events/upcomingEventsData/spring-2026-concentration-declaration-panel.jsx
@@ -1,13 +1,13 @@
-// export default {
-//   index: 1,
-//   title: "Concentration Declaration Panel",
-//   date: "March 5th, 2026",
-//   time: "6:00 PM - 7:00 PM",
-//   location: "MacMillan 115",
-//   link: "/events/spring-2026-concentration-declaration-panel",
-//   coverImage: "/events/spring-2026-concentration-declaration-panel/flier.jpg",
-//   flier: "/events/spring-2026-concentration-declaration-panel/flier.png",
-//   description:
-//     "WiSE hosted a panel of upperclassmen to share their journeys to choosing a concentration. Attendees asked questions, heard advice, and connected with peers.",
-//   imageFolder: "/events/spring-2026-concentration-declaration-panel",
-// };
+export default {
+  index: 1,
+  title: "Concentration Declaration Panel",
+  date: "March 5th, 2026",
+  time: "6:00 PM - 7:00 PM",
+  location: "MacMillan 115",
+  link: "/events/spring-2026-concentration-declaration-panel",
+  coverImage: "/events/spring-2026-concentration-declaration-panel/flier.jpg",
+  flier: "/events/spring-2026-concentration-declaration-panel/flier.png",
+  description:
+    "WiSE hosted a panel of upperclassmen to share their journeys to choosing a concentration. Attendees asked questions, heard advice, and connected with peers.",
+  imageFolder: "/events/spring-2026-concentration-declaration-panel",
+};
