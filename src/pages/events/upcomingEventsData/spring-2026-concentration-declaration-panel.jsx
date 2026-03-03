447ce0879371a2cdbd44d680dafeffacc0ac9bcd
danidedona <daniela_dedona@brown.edu>
2026-03-03 12:22:25 -0500

    commented out event

--- src/pages/events/upcomingEventsData/spring-2026-concentration-declaration-panel.jsx
+++ src/pages/events/upcomingEventsData/spring-2026-concentration-declaration-panel.jsx
@@ -1,15 +1,15 @@
-export default {
-  index: 1,
-  title: "Concentration Declaration Drop In Hours",
-  date: "March 5th, 2026",
-  time: "6:00 PM - 7:00 PM",
-  location: "MacMillan 115",
-  link: "/events/spring-2026-concentration-declaration-drop-in-hours",
-  coverImage:
-    "/events/spring-2026-concentration-declaration-drop-in-hours/flier.jpg",
-  flier:
-    "/events/spring-2026-concentration-declaration-drop-in-hours/flier.png",
-  description:
-    "WiSE hosted a panel of upperclassmen to share their journeys to choosing a concentration. Attendees asked questions, heard advice, and connected with peers.",
-  imageFolder: "/events/spring-2026-concentration-declaration-drop-in-hours",
-};
+// export default {
+//   index: 1,
+//   title: "Concentration Declaration Drop In Hours",
+//   date: "March 5th, 2026",
+//   time: "6:00 PM - 7:00 PM",
+//   location: "MacMillan 115",
+//   link: "/events/spring-2026-concentration-declaration-drop-in-hours",
+//   coverImage:
+//     "/events/spring-2026-concentration-declaration-drop-in-hours/flier.jpg",
+//   flier:
+//     "/events/spring-2026-concentration-declaration-drop-in-hours/flier.png",
+//   description:
+//     "WiSE hosted a panel of upperclassmen to share their journeys to choosing a concentration. Attendees asked questions, heard advice, and connected with peers.",
+//   imageFolder: "/events/spring-2026-concentration-declaration-drop-in-hours",
+// };
