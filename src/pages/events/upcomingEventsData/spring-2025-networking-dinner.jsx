0fde6cd0698c47d09b417dc57429d2243fab046b
danidedona <daniela_dedona@brown.edu>
2026-04-05 16:48:12 -0400

    event

---
+++ src/pages/events/upcomingEventsData/spring-2025-networking-dinner.jsx
@@ -0,0 +1,11 @@
+export default {
+  index: 0,
+  title: "2026 WiSE Networking Dinner",
+  date: "April 11th, 2026",
+  time: "5:00 PM - 8:00 PM",
+  location: "Faunce, 2nd Floor",
+  link: "/spring-conference-2026",
+  coverImage: "/events/spring-2026-networking-dinner/flier.png",
+  flier: "/events/spring-2026-networking-dinner/flier.png",
+  imageFolder: "/events/spring-2026-wise-headshots",
+};
